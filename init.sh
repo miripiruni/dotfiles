2f5c3ca183bdb76d173d786efbb1c6a6e49f7e79
Vyacheslav Oliyanchuk <miripiruni@yandex-team.ru>
2022-02-03 16:19:55 +0300

    init.sh: check osx and brew

--- init.sh
+++ init.sh
@@ -1,9 +1,24 @@
 # See https://github.com/ibraheemdev/modern-unix
-brew install bat # https://github.com/sharkdp/bat
-brew install exa # https://github.com/ogham/exa
-brew install dust # https://github.com/bootandy/dust
-brew install ripgrep # https://github.com/BurntSushi/ripgrep
-brew install jq # https://github.com/stedolan/jq
-brew install bottom # https://github.com/ClementTsang/bottom
-brew install procs # https://github.com/dalance/procs
-brew install zoxide # https://github.com/ajeetdsouza/zoxide
+declare -a tools=("bat" # https://github.com/sharkdp/bat
+            "exa" # https://github.com/ogham/exa
+            "dust" # https://github.com/bootandy/dust
+            "ripgrep" # https://github.com/BurntSushi/ripgrep
+            "jq" # https://github.com/stedolan/jq
+            "bottom" # https://github.com/ClementTsang/bottom
+            "procs" # https://github.com/dalance/procs
+            "zoxide" # https://github.com/ajeetdsouza/zoxide
+            )
+
+case $OSTYPE in darwin*)
+    case $(which brew) in "brew not found")
+        # https://brew.sh
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    esac
+
+    brew update
+
+    for tool in "${tools[@]}"
+    do
+        brew install "$tool"
+    done
+esac
