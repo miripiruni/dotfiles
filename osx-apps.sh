1dbea54ec4b74aa3dbb94c0d6decec39ff7edfaa
Vyacheslav Oliyanchuk <miripiruni@gmail.com>
2022-02-08 15:43:13 +0300

    osx apps via brew cask

---
+++ osx-apps.sh
@@ -0,0 +1,20 @@
+brew install --cask firefox
+brew install --cask spectacle
+brew install --cask vlc
+brew install --cask zoomus
+brew install --cask xmind-zen
+brew install --cask notion
+brew install --cask font-fira-code
+brew install --cask yandex-disk
+brew install --cask reaper
+brew install --cask fsnotes
+
+# alacritty
+# AnkiApp
+# clipy
+# karabiner
+# little-fingers
+# quick-plan
+# spotify
+# telegram
+
