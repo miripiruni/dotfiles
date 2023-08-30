30699d4b0b6e6ea352389f64d0bbe3a833017d78
miripiruni <miripiruni@gmail.com>
2023-08-30 14:54:50 +0200

    feature sliced configs

--- init.sh
+++ init.sh
@@ -43,5 +43,6 @@ case $OSTYPE in darwin*)
 
 
     https://ohmyz.sh/#install
+    # TODO: https://github.com/ohmyzsh/ohmyzsh/issues/9543
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 esac
