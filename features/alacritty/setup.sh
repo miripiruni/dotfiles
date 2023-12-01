e1b60f5e91c2365d3b03e616ab87aa0a0f566724
miripiruni <miripiruni@gmail.com>
2023-12-01 16:06:20 +0100

    fix: alacritty setup

--- features/alacritty/setup.sh
+++ features/alacritty/setup.sh
@@ -1,6 +1,21 @@
+# Depends on Fira Code Font
+../font-fira-code/setup.sh
+
 case $OSTYPE in darwin*)
     brew install --cask alacritty
 
-    mv $XDG_CONFIG_HOME/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml.default
-    ln -s ~/.dotfiles/features/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
+    ALACRITTY_CONFIG_DIR="$XDG_CONFIG_HOME/alacritty"
+    ALACRITTY_CONFIG_FILE="$ALACRITTY_CONFIG_DIR/alacritty.yml"
+
+    if [ -d "$ALACRITTY_CONFIG_DIR" ]; then
+        if [ -f "$ALACRITTY_CONFIG_FILE" ]; then
+            mv $ALACRITTY_CONFIG_FILE $XDG_CONFIG_HOME/alacritty/alacritty.yml.default
+        fi
+    else
+        mkdir $ALACRITTY_CONFIG_DIR
+        echo "Directory $ALACRITTY_CONFIG_DIR created."
+    fi
+
+    DIR=$(cd `dirname $0` && pwd)
+    ln -s $DIR/alacritty.yml $ALACRITTY_CONFIG_FILE
 esac
--- features/alacritty/uninstall.sh
+++ features/alacritty/uninstall.sh
@@ -1,4 +1,7 @@
+# Depends on Fira Code Font
+../font-fira-code/uninstall.sh
+
 brew uninstall alacritty
 
 echo "Removing configuration directory: ${HOME}/.config/alacritty…"
-rm -fr $HOME/.config/alacritty
+rm -fr $XDG_CONFIG_HOME/alacritty
