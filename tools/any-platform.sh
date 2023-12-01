126ef7c2221a63264163fd0f767b80c89875c042
miripiruni <miripiruni@gmail.com>
2023-12-01 15:26:19 +0100

    fix: use XDG dirs

--- tools/any-platform.sh
+++ tools/any-platform.sh
@@ -1,10 +1,9 @@
-# TODO: XDG dirs
-CONFIGS_BACKUP_DIR="$HOME/.configs-backup"
+CONFIGS_BACKUP_DIR="$XDG_CONFIG_HOME/.configs-backup"
 mkdir -p $CONFIGS_BACKUP_DIR
-mv ~/.zshrc $CONFIGS_BACKUP_DIR/.zshrc
-mv ~/.gitconfig $CONFIGS_BACKUP_DIR/.gitconfig
-mv ~/.bashrc $CONFIGS_BACKUP_DIR/.bashrc
-mv ~/.bash_profile $CONFIGS_BACKUP_DIR/.bash_profile
+mv $HOME/.zshrc $CONFIGS_BACKUP_DIR/.zshrc
+mv $HOME/.gitconfig $CONFIGS_BACKUP_DIR/.gitconfig
+mv $HOME/.bashrc $CONFIGS_BACKUP_DIR/.bashrc
+mv $HOME/.bash_profile $CONFIGS_BACKUP_DIR/.bash_profile
 
 echo ""
 echo "⚠️  Previous configuration files moved to ${CONFIGS_BACKUP_DIR}. After successful setup remove them please"
