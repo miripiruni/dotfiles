1fa729f7631b7b3d9db9d810f25d0902e8edd120
miripiruni <miripiruni@gmail.com>
2024-11-04 13:03:10 +0100

    fix: check if brew installed

--- features/brew/setup.sh
+++ features/brew/setup.sh
@@ -1,6 +1,8 @@
-case $(which brew) in "brew not found")
+which -s brew
+
+if [[ $? != 0 ]]; then
     # https://brew.sh
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-esac
+fi
 
 brew update
