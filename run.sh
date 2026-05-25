e3907a21063ef93e2636d6486b83b7e1bc583960
miripiruni <miripiruni@gmail.com>
2026-05-25 18:36:05 +0200

    refactor: bash case to conditional operators

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 # XDG Base Directories
 source $HOME/.dotfiles/.xdg.dirs
 
-case $OSTYPE in darwin*)
+if [[ $OSTYPE == darwin* ]]; then
     # Ensure brew is installed
     $HOME/.dotfiles/features/brew/setup.sh
-esac
+fi
 
 $HOME/.dotfiles/tools/any-platform.sh
 
-case $OSTYPE in darwin*)
+if [[ $OSTYPE == darwin* ]]; then
     ./tools/macos.sh
-esac
+fi
 
-case $OSTYPE in linux*)
+if [[ $OSTYPE == linux* ]]; then
     ./tools/linux.sh
-esac
+fi
